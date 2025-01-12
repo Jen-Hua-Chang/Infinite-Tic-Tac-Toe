810db37289e07f6351fe46df41dc98d63af6eb0f
Jen-Hua-Chang <jhchang434@gmail.com>
2025-01-13 00:45:06 +0800

    Remove files

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { ghPages } from 'vite-plugin-gh-pages';
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: "/Infinite-Tic-Tac-Toe/",
-  plugins: [react()],
-})
